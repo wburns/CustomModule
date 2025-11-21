68a24a798fcfae5db43885cd9a2277f0ae79b45e
William Burns <mudokonman@gmail.com>
2025-11-21 14:00:12 -0800

    Custom Module example

---
+++ src/main/java/myPackage/CustomModule.java
@@ -0,0 +1,31 @@
+package myPackage;
+
+import org.infinispan.Cache;
+import org.infinispan.configuration.global.GlobalConfiguration;
+import org.infinispan.factories.ComponentRegistry;
+import org.infinispan.factories.GlobalComponentRegistry;
+import org.infinispan.factories.annotations.InfinispanModule;
+import org.infinispan.lifecycle.ModuleLifecycle;
+
+@InfinispanModule(name = "custom-module", requiredModules = "core")
+public class CustomModule implements ModuleLifecycle {
+   @Override
+   public void cacheManagerStarting(GlobalComponentRegistry gcr, GlobalConfiguration globalConfiguration) {
+      CustomModuleConfiguration config = globalConfiguration.module(CustomModuleConfiguration.class);
+      // The config object will be null if the custom module hasn't been configured declaratively via JSON/XML/Yaml
+      // or programmatically when creating the EmbeddedCacheManager GlobalConfiguration
+      if (config != null) {
+         // Print out welcome message from CustomModuleConfiguration. If the message attribute hasn't been overridden in
+         // the config, then the default message is printed
+         System.out.println("Custom Module Message: " + config.message());
+      } else {
+         System.out.println("Custom Module namespace was not configured");
+      }
+   }
+
+   @Override
+   public void cacheStarted(ComponentRegistry cr, String cacheName) {
+      System.out.println("Cache " + cacheName + " started!");
+      cr.getComponent(Cache.class).addListener(new CustomListener());
+   }
+}
